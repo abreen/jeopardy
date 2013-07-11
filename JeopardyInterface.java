e81fd1aa140acfc4514ae28443bf71e875bea492
Alexander Breen <alexander.breen@gmail.com>
2013-07-11 19:49:35 -0400

    Added preliminary version of interface

---
+++ JeopardyInterface.java
@@ -0,0 +1,62 @@
+/**
+ * A specification for code that runs a Jeopardy! game.
+ * 
+ * @author Alexander Breen <alexander.breen@gmail.com>
+ */
+public interface JeopardyInterface {
+  
+  public static final int BOARD_WIDTH = 4;
+  public static final int BOARD_HEIGHT = 5;
+  
+  public static final int NUMBER_OF_TEAMS = 4;
+  
+  /**
+   * Whether the game should ask for teach team to come up
+   * with a name.
+   */
+  public static final boolean USE_TEAM_NAMES = false;
+  
+  /**
+   * The dollar amounts of the questions on each row, where the nth
+   * row on the board corresponds to the n - 1st element in this array.
+   */
+  public static final int[] DOLLAR_AMOUNTS = {100, 200, 300, 400, 500};
+  
+  /**
+   * Given a board coordinate, return the answer string. Remember: answer
+   * comes first!
+   * 
+   * @param x The x-coordinate of the tile
+   * @param y The y-coordinate of the tile
+   * @return The answer string
+   */
+  public String getAnswer(int x, int y);
+  
+  /**
+   * Given a board coordinate, return whether this tile is a Daily Double
+   * tile.
+   * 
+   * @param x The x-coordinate of the tile
+   * @param y The y-coordinate of the tile
+   * @return True if the tile is a Daily Double tile
+   */
+  public boolean isDailyDouble(int x, int y);
+  
+  /**
+   * Awards the specified dollar amount to the specified team.
+   * 
+   * @param team The team to which to award the money
+   * @param amount The dollar amount to award
+   */
+  public void correct(int team, int amount);
+  
+  /**
+   * Takes the specified dollar amount from the specified team.
+   * 
+   * @param team The team from which to take the money
+   * @param amount The dollar amount to take
+   */
+  public void incorrect(int team, int amount);
+  
+  
+}
